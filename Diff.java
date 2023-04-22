40db841638994f0eef24919809fce372d1b746ae
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2023-04-22 16:32:05 +0530

    New Diff.java

---
+++ Diff.java
@@ -0,0 +1,10 @@
+public class Diff {
+    void sum(){
+        int a=3,b=1;
+        System.out.println(a-b);
+    }
+    public static void main(String[] args) {
+        Diff d=new Diff();
+        d.sum();
+    }
+}
