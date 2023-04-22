8c8f3e8e96f0c7ae4e1f2bc3f073a4af8fb3f755
Vaibhav Chaudhary <vaibhavchaudhary329@gmail.com>
2023-04-23 00:19:00 +0530

    Initial Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+    void multiply(){
+        int a=3,b=1;
+        System.out.println(a*b);
+    }
+
+    public static void main(String[] args) {
+        Multiply m=new Multiply();
+        m.multiply();
+    }
+}
